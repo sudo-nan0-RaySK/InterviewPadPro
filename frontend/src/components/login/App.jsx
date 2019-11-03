8dc13cefe8354be668b8cbbc718060e8086e39fb
adirids <31789611+adirids@users.noreply.github.com>
2019-11-03 22:43:18 +0530

    Add files via upload

--- frontend/src/components/login/App.jsx
+++ frontend/src/components/login/App.jsx
@@ -1,79 +1,18 @@
 import React from "react";
 import "./App.scss";
-import { Login, Register, Interview } from "./components/login/index";
+import Nav from './components/Nav';
+import { Login, Register, Interview, Question } from "./components/login/index";
 import { BrowserRouter, Route,NavLink,Link} from "react-router-dom";
-const Nav = () => (
-  <div>
-    <ul>
-      <li><Link to="/interview">Interview</Link></li>
-    </ul>
-  </div>
-);
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLogginActive: true
-    };
-  }
-
-  componentDidMount() {
-    //Add .right by default
-    this.rightSide.classList.add("right");
-  }
-
-  changeState() {
-    const { isLogginActive } = this.state;
-
-    if (isLogginActive) {
-      this.rightSide.classList.remove("right");
-      this.rightSide.classList.add("left");
-    } else {
-      this.rightSide.classList.remove("left");
-      this.rightSide.classList.add("right");
-    }
-    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
-  }
-
   render() {
-    const { isLogginActive } = this.state;
-    const current = isLogginActive ? "Register" : "Login";
-    const currentActive = isLogginActive ? "login" : "register";
-    return (
+    return(
       <div className="App">
-        <div className="login">
-          <div className="container" ref={ref => (this.container = ref)}>
-            {isLogginActive && (
-              <Login containerRef={ref => (this.current = ref)} />
-            )}
-            {!isLogginActive && (
-              <Register containerRef={ref => (this.current = ref)} />
-            )}
-          </div>
-          <RightSide
-            current={current}
-            currentActive={currentActive}
-            containerRef={ref => (this.rightSide = ref)}
-            onClick={this.changeState.bind(this)}
-          />
-        </div>
-    </div>
-    );
-  }
+        <Nav/>
+				<div id="editor-pane">
+        <Question/>
+				</div>
+			</div>
+		);
+	}
 }
-
-const RightSide = props => {
-  return (
-    <div
-      className="right-side"
-      ref={props.containerRef}
-      onClick={props.onClick}
-    >
-      <div className="inner-container">
-        <div className="text">{props.current}</div>
-      </div>
-    </div>
-  );
-};
-
 export default App;
